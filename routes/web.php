19e1a0e42ea3c4be82e3c271f4f697e48db6c1a0
kuroneko52 <cruel.wonderland@outlook.com>
2025-06-19 18:13:44 +0900

    add book and author page resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('books', BookController::class)->except(['show']);
+Route::resource('authors', AuthorController::class)->except(['show']);
