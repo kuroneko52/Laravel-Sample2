97a87516eb0750cf4b7b16b3e72b7f8098d90985
kuroneko52 <cruel.wonderland@outlook.com>
2025-06-30 22:58:54 +0900

    import lang php

---
+++ lang/en/author.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'bookslist' => 'Books List',
+    'authorslist' => 'Authors List',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'addbook' => 'Add Book',
+    'addauthor' => 'Add Author',
+    'editbook' => 'Edit Book',
+    'editauthor' => 'Edit Author',
+    'title' => 'Title:',
+    'author' => 'Author:',
+    'name' => 'Name:',
+    'add' => 'Add',
+    'update' => 'Update',
+    'backtobookslist' => 'Back to Books List',
+    'backtoauthorslist' => 'Back to Authors List',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+];
---
+++ lang/ja/author.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'bookslist' => '本一覧',
+    'authorslist' => '作者一覧',
+    'edit' => '編集',
+    'delete' => '削除',
+    'addbook' => '本の追加',
+    'addauthor' => '作者の追加',
+    'editbook' => '本の編集',
+    'editauthor' => '作者の編集',
+    'title' => 'タイトル:',
+    'author' => '作者:',
+    'name' => '名前:',
+    'add' => '追加',
+    'update' => '更新',
+    'backtobookslist' => '本一覧へ戻る',
+    'backtoauthorslist' => '作者一覧へ戻る',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+];
