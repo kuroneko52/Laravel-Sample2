d309ab9a63509e6a4dd772a24bb4b12418c8644d
kuroneko52 <cruel.wonderland@outlook.com>
2025-07-10 09:44:24 +0900

    make localization code at Controller

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class LocaleController extends Controller
 {
     //
+    public function setLocale($locale)
+    {
+        // セッションにロケールを保存
+        session(['app_locale' => $locale]);
+
+        // リダイレクト先を指定（例: 前のページに戻る）
+        return redirect()->back();
+    }
 }
